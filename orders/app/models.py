23a8a799bfcca5af3d73dca05228715f02780d5c
cotten-blackwell <cotten.blackwell@gmail.com>
2015-11-16 13:09:23 -0800

    Starting to peel out original objects, etc.

--- orders/app/models.py
+++ orders/app/models.py
@@ -86,29 +86,6 @@ class Customer(db.Model):
         return self
 
 
-class Product(db.Model):
-    __tablename__ = 'products'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True)
-    items = db.relationship('Item', backref='product', lazy='dynamic')
-
-    def get_url(self):
-        return url_for('api.get_product', id=self.id, _external=True)
-
-    def export_data(self):
-        return {
-            'self_url': self.get_url(),
-            'name': self.name
-        }
-
-    def import_data(self, data):
-        try:
-            self.name = data['name']
-        except KeyError as e:
-            raise ValidationError('Invalid product: missing ' + e.args[0])
-        return self
-
-
 class Order(db.Model):
     __tablename__ = 'orders'
     id = db.Column(db.Integer, primary_key=True)
